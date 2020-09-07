a6c3052cb805a3199a7e7d2a695934288e626235
iminfinity <this.is.me.infinity@gmail.com>
2020-09-07 15:47:35 +0530

    Updates

---
+++ precache-manifest.deff752c96b52f2cee4988e055e6e73e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "652671ef02ea6c5a2795e9eb20fcd76f",
+    "url": "/quarantine-watchlist/index.html"
+  },
+  {
+    "revision": "801d64e5dc70afa2642a",
+    "url": "/quarantine-watchlist/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "37c60acb0bd8bf7e4401",
+    "url": "/quarantine-watchlist/static/js/2.f584a5fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quarantine-watchlist/static/js/2.f584a5fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "801d64e5dc70afa2642a",
+    "url": "/quarantine-watchlist/static/js/main.024ba6d0.chunk.js"
+  },
+  {
+    "revision": "f64be121e8ca879fc9f4",
+    "url": "/quarantine-watchlist/static/js/runtime-main.259608e4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quarantine-watchlist/static/media/logo.5d5d9eef.svg"
+  }
+]);
