21c84464049614e73a9fcf2bb5fafa646a0711ad
iminfinity <this.is.me.infinity@gmail.com>
2020-09-07 16:22:15 +0530

    modified header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,8 +5,12 @@ import { ReactComponent as Github } from "../../assets/github.svg";
 const Header = () => {
   return (
     <header className="header">
-      <h1>Quarantine-Watchlist</h1>
-      <a href="https://iminfinity.github.io/quarantine-watchlist/">
+      <h1>
+        <a href="https://iminfinity.github.io/quarantine-watchlist/">
+          Quarantine-Watchlist
+        </a>
+      </h1>
+      <a href="https://github.com/iminfinity/quarantine-watchlist">
         <Github />
       </a>
     </header>
